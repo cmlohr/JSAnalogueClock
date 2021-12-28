3cb5ec890e7ea65090de65a181ca04be4998ae76
Colleen Lohr <cmlohr@outlook.com>
2021-12-28 16:40:18 -0600

    Added doc file with main.html, main.js, index.css

---
+++ docs/main.js
@@ -0,0 +1,10 @@
+setInterval(function() {
+  var current = new Date();
+  var seconds = current.getHours() * 3600 + current.getMinutes() * 60 + current.getSeconds();
+  var hourRotate = "rotate(" + ((seconds % 43200) * 360 / 43200 + 180) + "deg)";
+  var minuteRotate = "rotate(" + ((seconds % 3600) * 360 / 3600 + 180) + "deg)";
+  var secondRotate = "rotate(" + ((seconds % 60) * 360 / 60 + 180) + "deg)";
+  $(".hour-hand").css("transform", hourRotate);
+  $(".min-hand").css("transform", minuteRotate);
+  $(".secs-hand").css("transform", secondRotate);
+}, 1000);
